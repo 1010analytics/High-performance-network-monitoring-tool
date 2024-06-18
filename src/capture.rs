b874a3ad3d105b42fb7a974c99d83df5f9422642
Tathya <data.scientist1010@gmail.com>
2024-06-18 22:01:31 +0530

    Initial commit

---
+++ src/capture.rs
@@ -0,0 +1,4 @@
+pub fn initialize() {
+    println!("Network packet capture initialized.");
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use tokio::main;
+
+mod analysis;
+mod capture;
+mod ui;
+
+#[tokio::main]
+async fn main() {
+    println!("Starting High-Performance Network Monitoring Tool...");
+}
